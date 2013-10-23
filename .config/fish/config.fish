ff1d403b029eaafda50af2efb378c1fd6a2eabfd
Jake Drahos <drahos.jake@gmail.com>
2013-10-23 15:01:09 -0500

    Added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+set fish_color_autosuggestion 808080
+set fish_color_command green\x1e--bold
+set fish_color_cwd yellow
+set fish_color_cwd_root yellow\x1e--bold
+set fish_color_normal white
+set fish_color_param blue\x1e--bold
+set fish_color_quote cyan\x1e--bold
+set fish_color_search_match magenta
+set fish_color_valid_path magenta\x1e--underline
+
+set fish_greeting (date)
+
+set PATH $HOME/.bin $PATH
+
+setenv EDITOR "vim"
+setenv VISUAL "vim"
