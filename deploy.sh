45c18db2e9194ec11c226b97540f2b8c5afc1fdd
Jake Drahos <jake@linux-h9mh.suse>
2016-05-16 16:52:55 -0500

    Added zsh to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -46,6 +46,20 @@ fi
 rm ~/.bash_prompt
 cp .bash_prompt ~/.bash_prompt
 
+# zsh
+if [ -e ~/.zshrc ]
+then
+	echo ".zshrc already exists, skipping zsh setup"
+else
+	echo "Installing Oh-My-Zsh"
+	pushd ~
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	popd
+	cp custom.zsh-theme ~/.oh-my-zsh/themes/
+	cp .zshrc ~/
+	echo "zsh setup complete"
+fi
+
 echo "Done!"
 echo "Run \`. ~/.bash_prompt' to load new bash prompt"
 echo "Old dotfiles in dotfiles.old"
